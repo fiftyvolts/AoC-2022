4ed63b68ca2ecd2c3ad45748bd50008dcf47a34f
fiftyvolts <fiftyvolts@example.com>
2022-11-29 20:57:45 -0500

    Initial Commit of boilerplate

---
+++ day14/src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, fs};
+
+fn input_txt() -> String {
+    let path = env::args().nth(1).unwrap_or(String::from("ex1.txt"));
+    fs::read_to_string(path).ok().unwrap_or_default()
+}
+
+fn main() {
+    let input = input_txt();
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &String) {
+    todo!();
+}
+
+fn part2(input: &String) {
+    todo!();
+}
