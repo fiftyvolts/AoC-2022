f2964b4071d86ec51d188f04aeda0b1b450f73e5
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-03 23:01:01 -0500

    Cleaned a bit

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -17,43 +17,38 @@ fn main() {
 }
 
 lazy_static! {
-    static ref SCORES: HashMap<u8, u32> = HashMap::from_iter(
-        (0..26)
-            .map(|i| [(b'a' + i, 1 + i as u32), (b'A' + i, 27 + i as u32)])
-            .flatten()
-    );
+    static ref SCORES: HashMap<u8, u32> =
+        HashMap::from_iter((b'a'..=b'z').chain(b'A'..=b'Z').zip(1..));
 }
 
 fn priority(l: &str) -> Option<&u32> {
-    let s: HashSet<u8> = HashSet::from_iter(l.bytes().take(l.len() / 2));
+    let s: HashSet<u8> = HashSet::from_iter(l[..l.len() / 2].bytes());
     (*SCORES).get(
-        &l.bytes()
-            .skip(l.len() / 2)
+        &l[l.len() / 2..]
+            .bytes()
             .filter(|x| s.contains(x))
             .next()
             .unwrap(),
     )
 }
 
-fn part1(input: &String) {
-    println!(
-        "{}",
-        input.lines().map(|l| priority(l).unwrap()).sum::<u32>()
-    );
+fn part1(inp: &String) {
+    println!("{}", inp.lines().map(|l| priority(l).unwrap()).sum::<u32>());
+}
+
+fn unique(items: &str) -> HashSet<u8> {
+    HashSet::from_iter(items.bytes())
 }
 
 fn part2(input: &String) {
     let lines = input.lines().collect::<Vec<&str>>();
     let mut score = 0;
-    for i in (0..lines.len()).step_by(3) {
-        let mut item_count: HashMap<u8, usize> = HashMap::new();
-        for j in 0..3 {
-            for key in HashSet::<u8>::from_iter(lines[i + j].bytes()) {
-                let count = item_count.entry(key).and_modify(|x| *x += 1).or_insert(1);
-                if *count == 3 {
-                    score += SCORES.get(&key).unwrap();
-                    break;
-                }
+    for group in lines.chunks(3) {
+        let mut freq: HashMap<u8, usize> = HashMap::new();
+        for key in group.iter().map(|g| unique(g)).flatten() {
+            if *freq.entry(key).and_modify(|x| *x += 1).or_insert(1) == 3 {
+                score += SCORES.get(&key).unwrap();
+                break;
             }
         }
     }
