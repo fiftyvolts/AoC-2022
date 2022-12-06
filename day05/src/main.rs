d25d7e6a3aa26e02c79ad8acda1fecff7a466d78
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-06 00:00:32 -0500

    minor cleanup

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, collections::VecDeque};
+use std::{collections::VecDeque, env, fs};
 
 fn input_txt() -> String {
     let path = env::args().nth(1).unwrap_or(String::from("ex1.txt"));
@@ -13,54 +13,80 @@ fn main() {
 
 fn part1(input: &String) {
     let lines: Vec<&str> = input.lines().collect();
-    let mut stacks: Vec<VecDeque<char>> = Vec::from_iter(std::iter::repeat(VecDeque::new()).take(9).clone());
+    let mut stacks: Vec<VecDeque<char>> =
+        Vec::from_iter(std::iter::repeat(VecDeque::new()).take(9).clone());
     let mut skip = 0;
-    for line in &lines {
-        if line.starts_with(" 1 ") { break; }
-        for (i, sack) in line.as_bytes().chunks(4).enumerate().filter(|p| p.1[1].is_ascii_alphabetic()) {
-           stacks[i].push_front(sack[1] as char);
-        }
-        skip+=1;
+    for line in lines.iter().take_while(|l| !l.starts_with(" 1")) {
+        line.as_bytes()
+            .chunks(4)
+            .enumerate()
+            .filter(|p| p.1[1].is_ascii_alphabetic())
+            .for_each(|(i, sack)| stacks[i].push_front(sack[1] as char));
+        skip += 1;
     }
-    skip+=2;
+    skip += 2;
 
     for line in &lines[skip..] {
-        let indexes: Vec<usize> = line.chars().filter(|c| c.is_numeric()||c.is_whitespace()).collect::<String>().split_whitespace().map(|s| usize::from_str_radix(s, 10).unwrap()).collect();
+        let indexes: Vec<usize> = line
+            .chars()
+            .filter(|c| c.is_numeric() || c.is_whitespace())
+            .collect::<String>()
+            .split_whitespace()
+            .map(|s| usize::from_str_radix(s, 10).unwrap())
+            .collect();
         for _ in 0..indexes[0] {
-            let temp = stacks[indexes[1]-1].pop_back().unwrap();
-            stacks[indexes[2]-1].push_back(temp);
+            let temp = stacks[indexes[1] - 1].pop_back().unwrap();
+            stacks[indexes[2] - 1].push_back(temp);
         }
     }
 
-    println!("{}", stacks.iter().map(|s| s.back().unwrap_or(&' ')).collect::<String>());
-
+    println!(
+        "{}",
+        stacks
+            .iter()
+            .map(|s| s.back().unwrap_or(&' '))
+            .collect::<String>()
+    );
 }
 
 fn part2(input: &String) {
     let lines: Vec<&str> = input.lines().collect();
-    let mut stacks: Vec<VecDeque<char>> = Vec::from_iter(std::iter::repeat(VecDeque::new()).take(9).clone());
+    let mut stacks: Vec<VecDeque<char>> =
+        Vec::from_iter(std::iter::repeat(VecDeque::new()).take(9).clone());
     let mut skip = 0;
-    for line in &lines {
-        if line.starts_with(" 1 ") { break; }
-        for (i, sack) in line.as_bytes().chunks(4).enumerate().filter(|p| p.1[1].is_ascii_alphabetic()) {
-           stacks[i].push_front(sack[1] as char);
-        }
-        skip+=1;
+    for line in lines.iter().take_while(|l| !l.starts_with(" 1")) {
+        line.as_bytes()
+            .chunks(4)
+            .enumerate()
+            .filter(|p| p.1[1].is_ascii_alphabetic())
+            .for_each(|(i, sack)| stacks[i].push_front(sack[1] as char));
+        skip += 1;
     }
-    skip+=2;
+    skip += 2;
 
     for line in &lines[skip..] {
-        let indexes: Vec<usize> = line.chars().filter(|c| c.is_numeric()||c.is_whitespace()).collect::<String>().split_whitespace().map(|s| usize::from_str_radix(s, 10).unwrap()).collect();
+        let indexes: Vec<usize> = line
+            .chars()
+            .filter(|c| c.is_numeric() || c.is_whitespace())
+            .collect::<String>()
+            .split_whitespace()
+            .map(|s| usize::from_str_radix(s, 10).unwrap())
+            .collect();
         let mut temp = vec![];
         for _ in 0..indexes[0] {
-            temp.push(stacks[indexes[1]-1].pop_back().unwrap());
+            temp.push(stacks[indexes[1] - 1].pop_back().unwrap());
         }
         temp.reverse();
         for c in temp {
-        stacks[indexes[2]-1].push_back(c);
+            stacks[indexes[2] - 1].push_back(c);
         }
-        
     }
 
-    println!("{}", stacks.iter().map(|s| s.back().unwrap_or(&' ')).collect::<String>());
+    println!(
+        "{}",
+        stacks
+            .iter()
+            .map(|s| s.back().unwrap_or(&' '))
+            .collect::<String>()
+    );
 }
