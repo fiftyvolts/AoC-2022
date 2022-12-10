0e43320389ab0ee9180429b5bccdffbbe857a03a
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-10 10:17:32 -0500

    different way of reading input for day11 and 12

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,20 +1,23 @@
+#[macro_use]
+extern crate lazy_static;
 use std::{env, fs};
 
-fn input_txt() -> String {
-    let path = env::args().nth(1).unwrap_or(String::from("ex1.txt"));
-    fs::read_to_string(path).ok().unwrap_or_default()
+lazy_static! {
+    static ref INPUT: String = env::args()
+        .nth(1)
+        .and_then(|p| fs::read_to_string(p).ok())
+        .unwrap();
 }
 
 fn main() {
-    let input = input_txt();
-    part1(&input);
-    part2(&input);
+    part1();
+    //part2();
 }
 
-fn part1(input: &String) {
+fn part1() {
     todo!();
 }
 
-fn part2(input: &String) {
-    todo!();
-}
+// fn part2() {
+//     todo!();
+// }
