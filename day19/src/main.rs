42a37577f268fb7201ed6c25be2d64d8ca4c428e
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-19 10:42:12 -0500

    set up for a memoized brute force

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,35 +1,34 @@
 #[macro_use]
 extern crate lazy_static;
-use std::{env::var, fs::read_to_string};
+use std::{collections::HashMap, env::var, fs::read_to_string};
 
 use regex::Regex;
 
 lazy_static! {
     static ref INPUT: String = read_to_string(var("INPUT").unwrap()).unwrap();
     static ref RE : Regex = Regex::new(r"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.").unwrap();
+    static ref SCALE : i32 = i32::from_str_radix(var("SCALE").unwrap_or(String::from("100")).as_str(), 10).unwrap();
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct Cost {
     ore: i32,
     clay: i32,
     glass: i32,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Robot {
-    Ore(Cost),
-    Clay(Cost),
-    Glass(Cost),
-    Crystal(Cost),
+    Ore,
+    Clay,
+    Glass,
+    Crystal,
 }
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct Blueprint {
     id: i32,
-    costs: Vec<Robot>,
-    relative_value: Vec<i32>,
-    produced: i32,
+    costs: HashMap<Robot, Cost>
 }
 
 impl From<&str> for Blueprint {
@@ -42,41 +41,52 @@ impl From<&str> for Blueprint {
             .map(|m| i32::from_str_radix(m.unwrap().as_str(), 10).unwrap())
             .collect::<Vec<_>>();
 
+        let ore_bot = Cost {
+            ore: caps[1],
+            clay: 0,
+            glass: 0,
+        };
+
+        let clay_bot = Cost {
+            ore: caps[2],
+            clay: 0,
+            glass: 0,
+        };
+
+        let glass_bot = Cost {
+            ore: caps[3],
+            clay: caps[4],
+            glass: 0,
+        };
+
+        let crystal_bot = Cost {
+            ore: caps[5],
+            clay: 0,
+            glass: caps[6],
+        };
         Blueprint {
             id: caps[0],
-            costs: vec![
-                Robot::Ore(Cost {
-                    ore: caps[1],
-                    clay: 0,
-                    glass: 0,
-                }),
-                Robot::Clay(Cost {
-                    ore: caps[2],
-                    clay: 0,
-                    glass: 0,
-                }),
-                Robot::Glass(Cost {
-                    ore: caps[3],
-                    clay: caps[4],
-                    glass: 0,
-                }),
-                Robot::Crystal(Cost {
-                    ore: caps[5],
-                    clay: 0,
-                    glass: caps[6],
-                }),
-            ],
-            relative_value: vec![],
-            produced: 0,
+            costs: HashMap::from([
+                (Robot::Ore, ore_bot),
+                (Robot::Clay, clay_bot),
+                (Robot::Glass, glass_bot),
+                (Robot::Crystal, crystal_bot),
+            ]),
         }
     }
 }
 
+fn calculate_output(bp: &Blueprint) -> i32 {
+0
+}
 fn main() {
-    let mut blueprints = vec![];
-    for line in INPUT.lines() {
-        blueprints.push(Blueprint::from(line));
+    let blueprints = INPUT.lines().map(|l| Blueprint::from(l)).collect::<Vec<_>>();
+    let mut results = vec![];
+    for bp in &blueprints {
+        results.push((bp.id, calculate_output(bp)));
     }
 
-    println!("{:?}", blueprints);
+    results.sort_by(|a, b| a.1.cmp(&b.1));
+
+    println!("{:?}", results);
 }
