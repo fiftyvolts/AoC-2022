3e713d3fa8e378c8d49067410eaf17b412c499e7
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-21 06:43:04 -0500

    unfinished work I'll come back to

--- day20/blah.pl
+++ day20/blah.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
-
+use strict;
 local $" = ",";
+
 my $i    = 0;
-my @nums = map { chomp $_; [ $i++, $_ ] } (<ARGV>);
-print "0: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), "\n"
-  if $ENV{DEBUG};
+my @nums = map {
+    chomp $_;
+    if ( $_ ne "" ) { [ $i++, $_ ] }
+    else            { () }
+} (<ARGV>);
+print "0: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), "\n" if $ENV{DEBUG} > 1;
 
 my $next = 0;
 my $idx  = 0;
@@ -22,9 +26,15 @@ while ( $next < @nums ) {
     my $nidx;
     if ( $t->[1] > 0 ) {
         $nidx = ( $idx + $t->[1] ) % @nums;
+        if ( $nidx == $#nums ) {
+            $nidx = 0;
+        }
     }
     elsif ( $t->[1] < 0 ) {
-        $nidx = ( $idx + $t->[1] - 1 ) % @nums;
+        $nidx = ( ( $idx + $t->[1] ) % @nums ) - 1;
+        if ( $nidx == -1 ) {
+            $nidx = $#nums;
+        }
     }
     else {
         $idx++;
@@ -33,38 +43,43 @@ while ( $next < @nums ) {
     }
 
     if ( $nidx < $idx ) {
-        @nums = (
-            @nums[ 0 .. $nidx ],
-            $t,
-            @nums[ $nidx + 1 .. $idx - 1 ],
-            @nums[ $idx + 1 .. $#nums ]
-        );
+        if ( $idx < $#nums ) {
+            @nums = ( @nums[ 0 .. $nidx ], $t, @nums[ $nidx + 1 .. $idx - 1 ], @nums[ $idx + 1 .. $#nums ] );
+        }
+        else {
+            @nums = ( @nums[ 0 .. $nidx ], $t, @nums[ $nidx + 1 .. $idx - 1 ] );
+        }
     }
     elsif ( $nidx > $idx ) {
-        @nums = (
-            @nums[ 0 .. $idx - 1 ],
-            @nums[ $idx + 1 .. $nidx ],
-            $t, @nums[ $nidx + 1 .. $#nums ]
-        );
+        if ( $idx > 0 ) {
+            @nums = ( @nums[ 0 .. $idx - 1 ], @nums[ $idx + 1 .. $nidx ], $t, @nums[ $nidx + 1 .. $#nums ] );
+        }
+        else {
+            @nums = ( @nums[ $idx + 1 .. $nidx ], $t, @nums[ $nidx + 1 .. $#nums ] );
+        }
     }
+    print "$next: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), " - moved idx: $idx, ndx: $nidx, dat: $t->[1]]\n"
+      if $ENV{DEBUG} > 1;
     $next++;
     $idx = 0;
-    print "$next: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), "\n"
-      if $ENV{DEBUG};
 }
 
 die "didn't find it all" if $next != @nums;
 
-print "0: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), "\n"
-  if $ENV{DEBUG};
+if ( $ENV{DEBUG} ) {
+    for my $i ( 0 .. $#nums ) {
+        print "$i: @{[$nums[$i]->[1]]}\n";
+    }
+}
+
 my $zero;
-for my $idx ( 0 .. $#nums ) {
+for $idx ( 0 .. $#nums ) {
     if ( $nums[$idx]->[1] == 0 ) {
         $zero = $idx;
     }
 }
 die "couldn't find zero" if $zero >= @nums;
 print "zero at $zero\n";
-@final_idx = map { ( $_ + $zero ) % @nums } ( 1000, 2000, 3000 );
-@output    = map { $nums[$_]->[1] } @final_idx;
-print "@output ", $output[0] + $output[1] + $output[2], "\n";
+my @final_idx = map { ( $_ + $zero ) % @nums } ( 1000, 2000, 3000 );
+my @output    = map { $nums[$_]->[1] } @final_idx;
+print "@final_idx -> @output ", $output[0] + $output[1] + $output[2], "\n";
