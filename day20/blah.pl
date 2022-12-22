937165def638733dd1f5d4f12a24c98f2e1e46c3
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-22 15:41:38 -0500

    day20 starting part 2

--- day20/blah.pl
+++ day20/blah.pl
@@ -1,82 +1,74 @@
 #!/usr/bin/perl
 use strict;
-local $" = ",";
+use Data::Dumper;
 
-my $i    = 0;
+$Data::Dumper::Indent = 0;
+$Data::Dumper::Terse = 1;
+local $" = ",";
+my $i = 0;
+
 my @nums = map {
     chomp $_;
     if ( $_ ne "" ) { [ $i++, $_ ] }
     else            { () }
 } (<ARGV>);
-print "0: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), "\n" if $ENV{DEBUG} > 1;
+
+my $size   = scalar @nums;
+my $factor = $ENV{FACTOR} || 1;
+my $rounds = $ENV{ROUNDS}             || 1;
+my @nums   = map { [ $_->[0], $_->[1] * $factor ] } @nums;
+
+print "0: ", Dumper( \@nums ), "\n" if $ENV{DEBUG} > 1;
 
 my $next = 0;
 my $idx  = 0;
+for my $z (0..($rounds - 1)) {
+ $next = 0;
+ $idx  = 0;
+ print "round $z: ", Dumper( \@nums ), "\n" if $ENV{DEBUG} >=1;
+    while ( $next < @nums ) {
+        my $t = $nums[$idx];
 
-while ( $next < @nums ) {
-
-    # print "$idx $next\n";
-    my $t = $nums[$idx];
-
-    if ( $t->[0] != $next ) {
-        $idx++;
-        next;
-    }
-
-    my $dx = $t->[1];
-    if ($dx > @nums ) {
-       $dx = ($dx % @nums) + int($dx/@nums);
-    } eslif ( $dx < -@nums) {
-        $dx = ($dx % @nums) - int($idx/@nums);
-    }
-
-    my $nidx;
-    if ( $dx > 0 ) {
-        $nidx = ( $idx + $dx ) % @nums;
-        if ( $nidx == $#nums ) {
-            $nidx = 0;
+        if ( $t->[0] != $next ) {
+            $idx++;
+            next;
         }
-    }
-    elsif ( $dx < 0 ) {
-        $nidx = ( ( $idx + $dx->[1] ) % @nums ) - 1;
-        if ( $nidx == -1 ) {
-            $nidx = $#nums;
+
+        if ( $idx == 0 ) {
+            @nums = @nums[ 1 .. $#nums ];
         }
-    }
-    else {
-        $idx++;
+        elsif ( $idx == $#nums ) {
+            @nums = @nums[ 0 .. $idx - 1 ];
+        }
+        else {
+            @nums = ( @nums[ 0 .. $idx - 1 ], @nums[ $idx + 1 .. $#nums ] );
+        }
+
+        my $nidx;
+        if ( $t->[1] >= 0 ) {
+            $nidx = ( $idx + $t->[1] ) % @nums;
+        }
+        else {
+            $nidx = ( ( $idx + $t->[1] ) % @nums );
+        }
+
+        if ( $nidx == 0 ) {
+            @nums = ( $t, @nums );
+        }
+        elsif ( $nidx == $#nums ) {
+            @nums = ( @nums, $t );
+        }
+        else {
+            @nums = ( @nums[ 0 .. $nidx - 1 ], $t, @nums[ $nidx .. $#nums ] );
+        }
+
+        print "$next: ", Dumper( \@nums ), " - moved idx: $idx, ndx: $nidx, dat: $t->[1]]\n"
+          if $ENV{DEBUG} >= 2;
+
         $next++;
-        next;
+        $idx = 0;
     }
-
-    if ( $nidx < $idx ) {
-        if ( $idx < $#nums ) {
-            @nums = ( @nums[ 0 .. $nidx ], $t, @nums[ $nidx + 1 .. $idx - 1 ], @nums[ $idx + 1 .. $#nums ] );
-        }
-        else {
-            @nums = ( @nums[ 0 .. $nidx ], $t, @nums[ $nidx + 1 .. $idx - 1 ] );
-        }
-    }
-    elsif ( $nidx > $idx ) {
-        if ( $idx > 0 ) {
-            @nums = ( @nums[ 0 .. $idx - 1 ], @nums[ $idx + 1 .. $nidx ], $t, @nums[ $nidx + 1 .. $#nums ] );
-        }
-        else {
-            @nums = ( @nums[ $idx + 1 .. $nidx ], $t, @nums[ $nidx + 1 .. $#nums ] );
-        }
-    }
-    print "$next: ", join( ',', map { sprintf "%3d", $_->[1] } @nums ), " - moved idx: $idx, ndx: $nidx, dat: $t->[1]]\n"
-      if $ENV{DEBUG} > 1;
-    $next++;
-    $idx = 0;
-}
-
-die "didn't find it all" if $next != @nums;
-
-if ( $ENV{DEBUG} ) {
-    for my $i ( 0 .. $#nums ) {
-        print "$i: @{[$nums[$i]->[1]]}\n";
-    }
+    die "didn't shuffle all numbers" if $next != @nums;
 }
 
 my $zero;
@@ -85,8 +77,9 @@ for $idx ( 0 .. $#nums ) {
         $zero = $idx;
     }
 }
-die "couldn't find zero" if $zero >= @nums;
+die "couldn't find zero" if !defined($zero);
 print "zero at $zero\n";
+
 my @final_idx = map { ( $_ + $zero ) % @nums } ( 1000, 2000, 3000 );
 my @output    = map { $nums[$_]->[1] } @final_idx;
 print "@final_idx -> @output ", $output[0] + $output[1] + $output[2], "\n";
