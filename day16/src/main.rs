72f18c1da8e269a681e37d50e304913a98474eb1
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-29 19:35:26 -0500

    finally a fast solution

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,12 +1,10 @@
 #[macro_use]
 extern crate lazy_static;
 use std::{
-    collections::{BTreeSet, HashMap, HashSet, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     env::var,
     fs::read_to_string,
-    hash::Hash,
-    ops::Index,
-    time::SystemTime
+    time::SystemTime,
 };
 
 use regex::Regex;
@@ -16,36 +14,60 @@ lazy_static! {
     static ref PAT: Regex =
         Regex::new(r"Valve (\S\S) has flow rate=(\d+); tunnels? leads? to valves? (.*)").unwrap();
     static ref ELEPHANT: bool = var("ELEPHANT").is_ok();
-    static ref START_TIME : SystemTime = SystemTime::now();
+    static ref START_TIME: SystemTime = SystemTime::now();
 }
 
-#[derive(Debug, Clone)]
-struct Valve<'a> {
-    name: &'a str,
+#[derive(Debug, Clone, Copy)]
+struct Valve {
+    _name: u64,
     rate: i32,
-    adj: Vec<&'a str>,
+    adj: u64,
 }
 
-impl<'a> From<&'a str> for Valve<'a> {
-    fn from(input: &'a str) -> Self {
-        let caps = PAT.captures(&input).unwrap();
-        Valve {
-            name: caps.get(1).unwrap().as_str().into(),
-            rate: i32::from_str_radix(caps.get(2).unwrap().as_str(), 10).unwrap(),
-            adj: caps.get(3).unwrap().as_str().split(", ").collect(),
-        }
-    }
-}
+type Cave = HashMap<u64, Valve>;
 
 fn main() {
-    let mut cave: HashMap<&str, Box<Valve>> = HashMap::new();
+    let mut lut: HashMap<String, u64> = HashMap::new();
+    let mut last = 0u64;
+    let mut cave = Cave::new();
+
     for line in INPUT.lines() {
-        let valve = Box::new(Valve::from(line));
-        cave.insert(valve.name, valve);
+        let caps = PAT.captures(&line).unwrap();
+        let name = caps[1].to_string();
+        let rate = i32::from_str_radix(&caps[2], 10).unwrap();
+        let adj = caps[3]
+            .split(", ")
+            .map(|s| s.to_owned())
+            .collect::<Vec<_>>();
+
+        if !lut.contains_key(&name) {
+            lut.insert(name.clone(), 1 << last);
+            last += 1;
+        }
+
+        for valve in &adj {
+            if !lut.contains_key(valve) {
+                lut.insert(valve.clone(), 1 << last);
+                last += 1;
+            }
+        }
+
+        cave.insert(
+            lut[&name],
+            Valve {
+                _name: lut[&name],
+                rate,
+                adj: (&adj)
+                    .iter()
+                    .map(|s| lut[s])
+                    .reduce(|acc, i| acc | i)
+                    .unwrap(),
+            },
+        );
     }
 
-    let mut travel: HashMap<(&str, &str), i32> = HashMap::new();
-    let valve_names = cave.keys().collect::<Vec<_>>();
+    let mut travel: HashMap<(u64, u64), i32> = HashMap::new();
+    let valve_names = cave.keys().cloned().collect::<Vec<_>>();
     for i in 0..valve_names.len() {
         for j in 0..valve_names.len() {
             let start = valve_names[i];
@@ -56,89 +78,112 @@ fn main() {
             }
 
             let mut que = VecDeque::from([(0, start)]);
-            let mut visited = HashSet::<&str>::new();
+            let mut visited = HashSet::<u64>::new();
             while !que.is_empty() {
                 let (dist, curr) = que.pop_front().unwrap();
                 if curr == end {
                     travel.insert((start, curr), dist);
                     break;
                 }
-                for adj in &cave.get(curr).unwrap().adj {
-                    if !visited.contains(adj) {
-                        visited.insert(adj);
-                        que.push_back((dist + 1, adj));
+                for i in 0..64 {
+                    let adj = 1 << i;
+                    if cave[&curr].adj & adj != 0 {
+                        if !visited.contains(&adj) {
+                            visited.insert(adj);
+                            que.push_back((dist + 1, adj));
+                        }
                     }
                 }
             }
         }
     }
 
-    all_possible_paths(&cave, &travel);
+    let start = lut[&String::from("AA")];
+    all_possible_paths(&cave, &travel, start);
 }
 
-fn all_possible_paths(cave: &HashMap<&str, Box<Valve>>, travel: &HashMap<(&str, &str), i32>) {
-    let valve_names = cave
+fn all_possible_paths(cave: &Cave, travel: &HashMap<(u64, u64), i32>, start: u64) {
+    let valve_set = cave
         .keys()
-        .filter(|k| **k != "AA" && cave[*k].rate > 0)
+        .filter(|k| **k != start && cave[*k].rate > 0)
         .cloned()
-        .collect::<BTreeSet<_>>();
+        .reduce(|acc, k| acc | k)
+        .unwrap();
 
     println!(
         "Max solo {}",
-        dfs_solo(cave, travel, "AA", valve_names.clone(), 30, 0)
+        dfs_solo(cave, travel, start, valve_set, 30, 0)
     );
 
     println!(
         "Max pair {}",
-        search_pair(cave, travel, "AA", valve_names, 26)
+        search_pair(cave, travel, start, valve_set, 26)
     );
 }
 
-fn search_pair<'a>(
-    cave: &HashMap<&str, Box<Valve>>,
-    travel: &HashMap<(&str, &str), i32>,
-    curr_node: &str,
-    remaining: BTreeSet<&'a str>,
-    time: i32
+
+fn search_pair(
+    cave: &Cave,
+    travel: &HashMap<(u64, u64), i32>,
+    curr_node: u64,
+    remaining: u64,
+    time: i32,
 ) -> i32 {
     let mut max_released = 0;
 
-    let valve_order = Vec::from_iter(remaining);
+    let bits = (0..64)
+        .map(|i| 1 << i)
+        .filter(|b| b & remaining != 0)
+        .collect::<Vec<_>>();
 
-    for order in PermutationIter::new(valve_order.len()) {
-        let remaining1 = order[..order.len() / 2]
+    for order in CombinationIter::new(bits.len(), bits.len() / 2) {
+        let remaining1 = (&order)
             .iter()
-            .map(|i| valve_order[*i].clone())
-            .collect::<BTreeSet<_>>();
+            .map(|i| bits[*i])
+            .reduce(|acc, v| acc | v)
+            .unwrap();
+        
+        let release1 = dfs_solo(cave, travel, curr_node, remaining1, time, 0);
 
-        let mut next_released = dfs_solo(cave, travel, curr_node, remaining1, time, 0);
-
-        let remaining2 = order[order.len() / 2..]
+        let remaining2 = (&bits)
             .iter()
-            .map(|i| valve_order[*i].clone())
-            .collect::<BTreeSet<_>>();
-        next_released += dfs_solo(cave, travel, curr_node, remaining2, time, 0);
+            .filter(|b| remaining1 & **b == 0)
+            .cloned()
+            .reduce(|acc, v| acc | v)
+            .unwrap();
 
-        if next_released > max_released {
-            println!("New max pair {} (.{})", next_released, START_TIME.elapsed().unwrap().as_millis());
-            max_released = next_released;
+        let release2 = dfs_solo(cave, travel, curr_node, remaining2, time, 0);
+
+        if release1 + release2 > max_released {
+            println!(
+                "New max pair {}+{}={} (.{})",
+                release1,
+                release2,
+                release1 + release2,
+                START_TIME.elapsed().unwrap().as_millis()
+            );
+            max_released = release1 + release2;
         }
     }
 
     max_released
 }
 
-fn dfs_solo<'a>(
-    cave: &HashMap<&str, Box<Valve>>,
-    travel: &HashMap<(&str, &str), i32>,
-    curr_node: &str,
-    remaining: BTreeSet<&'a str>,
+fn dfs_solo(
+    cave: &Cave,
+    travel: &HashMap<(u64, u64), i32>,
+    curr_node: u64,
+    remaining: u64,
     time: i32,
-    best_so_far: i32
+    best_so_far: i32,
 ) -> i32 {
-    let mut release_ceiling = cave[curr_node].rate * time;
-    for valve in &remaining {
-        release_ceiling += (cave[valve].rate * (time - travel[&(curr_node, *valve)] - 1)).max(0);
+    let mut release_ceiling = cave[&curr_node].rate * time;
+    for i in 0..64 {
+        let valve = 1 << i;
+        if valve & remaining != 0 {
+            release_ceiling +=
+                (cave[&valve].rate * (time - travel[&(curr_node, valve)] - 1)).max(0);
+        }
     }
 
     if release_ceiling < best_so_far {
@@ -146,20 +191,25 @@ fn dfs_solo<'a>(
     }
 
     let mut max_released = 0;
-    for next_node in &remaining {
-        let next_time = time - travel[&(curr_node, *next_node)] - 1;
+    for i in 0..64 {
+        let next_node = 1 << i;
+        if next_node & remaining == 0 {
+            continue;
+        }
+
+        let next_time = time - travel[&(curr_node, next_node)] - 1;
         if next_time > 0 {
-            let mut next_remaining = remaining.clone();
-            next_remaining.remove(next_node);
+            let mut next_remaining = remaining;
+            next_remaining &= !next_node;
 
             let next_released = dfs_solo(
-                    cave,
-                    travel,
-                    next_node,
-                    next_remaining,
-                    next_time,
-                    max_released
-                );
+                cave,
+                travel,
+                next_node,
+                next_remaining,
+                next_time,
+                max_released,
+            );
 
             if next_released > max_released {
                 max_released = next_released;
@@ -167,87 +217,57 @@ fn dfs_solo<'a>(
         }
     }
 
-    let ret = max_released + cave[curr_node].rate * time;
+    let ret = max_released + cave[&curr_node].rate * time;
     ret
 }
 
-struct PermutationIter {
+struct CombinationIter {
+    size: usize,
+    subset: usize,
     regs: Vec<usize>,
     done: bool,
 }
 
-impl PermutationIter {
-    fn new(size: usize) -> PermutationIter {
-        PermutationIter {
-            regs: vec![0; size],
+impl CombinationIter {
+    fn new(size: usize, subset: usize) -> Self {
+        CombinationIter {
+            size,
+            subset,
+            regs: (0..).take(subset).collect(),
             done: false,
         }
     }
 }
 
-impl Iterator for PermutationIter {
+impl Iterator for CombinationIter {
     type Item = Vec<usize>;
+
     fn next(&mut self) -> Option<Self::Item> {
         if self.done {
             return None;
         }
-        let mut indexes: Vec<usize> = (0..self.regs.len()).collect();
-        let ret = self.regs.iter().map(|i| indexes.remove(*i)).collect();
 
-        let mut carry = 1;
-        for i in 0..self.regs.len() {
-            self.regs[i] = self.regs[i] + carry;
-            if self.regs[i] >= (self.regs.len() - i) {
-                self.regs[i] = 0;
-                carry = 1;
-            } else {
-                carry = 0;
+        let ret = self.regs.clone();
+
+        
+        while let Some(reg) = self.regs.pop() {
+            let next = reg + 1;
+            if next <= self.size - (self.subset - self.regs.len()) {
+                self.regs.push(next);
+                break;
             }
         }
 
-        if carry == 1 {
+        if self.regs.is_empty() {
             self.done = true;
+            return Some(ret);
+        }
+
+        while self.regs.len() < self.subset {
+            let start = self.regs.last().unwrap() + 1;
+            self.regs.push(start);
         }
 
         Some(ret)
     }
 }
-
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// struct NamedBitSet<'a, T>
-// where
-//     T: Index<&'a str, Output = u64>,
-// {
-//     set: u64,
-//     names: &'a T,
-// }
-
-// impl<'a, T> NamedBitSet<'a, T>
-// where
-//     T: Index<&'a str, Output = u64>,
-// {
-//     fn new(names: &'a T) -> Self {
-//         Self { set: 0, names }
-//     }
-
-//     fn insert(&mut self, item: &'a str) {
-//         self.set |= self.names[item];
-//     }
-
-//     fn remove(&mut self, item: &'a str) {
-//         self.set &= !(self.names[item]);
-//     }
-
-//     fn contains(&self, item: &'a str) -> bool {
-//         self.set & self.names[item] > 0
-//     }
-// }
-
-// impl<'a, T> Hash for NamedBitSet<'a, T>
-// where
-//     T: Index<&'a str, Output = u64>,
-// {
-//     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-//         self.set.hash(state);
-//     }
-// }
